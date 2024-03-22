5b26b45546f2c22e251b5699041b0fbb394e61b4
W. James Dittmar <wjdittmar@gmail.com>
2024-03-22 15:21:49 -0700

    update file structure part 4

--- part3_course/models/note.js
+++ part3_course/models/note.js
@@ -1,40 +1,20 @@
-const mongoose = require("mongoose");
-
-const url = process.env.MONGODB_URI;
-
-mongoose.set("strictQuery", false);
-mongoose
-  .connect(url)
-  .then((result) => {
-    console.log("connected to MongoDB");
-  })
-  .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
+const mongoose = require('mongoose')
 
 const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
-});
-
-// this updates it so that when you call the toJSON method
-// or any method that calls toJSON on the noteSchema model
-// that you transform the resulting output
-// to remove the _id field
-// and the __v field
-
-noteSchema.set("toJSON", {
-  transform: (document, returnedObject) => {
-    // this seems kind of magic that output
-    // will automatically create the id field even if it does not exist
-    // e.g.
-    // returnedObject.fakeField = returnedObject._id.toString();
-    // would create a fakeField variable in the JSON output
-
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+  content: {
+    type: String,
+    required: true,
+    minlength: 5
   },
-});
+  important: Boolean,
+})
 
-module.exports = mongoose.model("Note", noteSchema);
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Note', noteSchema)
