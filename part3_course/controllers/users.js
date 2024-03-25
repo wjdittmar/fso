30dd6281735c8098810162d4be02ba9fad5209fa
W. James Dittmar <wjdittmar@gmail.com>
2024-03-25 14:22:27 -0700

    4.8

--- part3_course/controllers/users.js
+++ part3_course/controllers/users.js
@@ -18,7 +18,8 @@ usersRouter.post('/', async (request, response) => {
     response.status(201).json(savedUser)
 })
 usersRouter.get('/', async (request, response) => {
-    const users = await User.find({})
+    const users = await User
+        .find({}).populate('notes', { content: 1, important: 1 })
     response.json(users)
 })
 module.exports = usersRouter
