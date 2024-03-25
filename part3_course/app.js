39774d6bc2c9714602c4c67b7d6c5bd5ffe0012a
W. James Dittmar <wjdittmar@gmail.com>
2024-03-25 14:47:21 -0700

    add user authentication and session token

--- part3_course/app.js
+++ part3_course/app.js
@@ -6,6 +6,8 @@ const cors = require('cors')
 
 const notesRouter = require('./controllers/notes')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+
 
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
@@ -28,6 +30,7 @@ app.use(express.static('dist'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 
+app.use('/api/login', loginRouter)
 app.use('/api/notes', notesRouter)
 app.use('/api/users', usersRouter)
 app.use(middleware.unknownEndpoint)
