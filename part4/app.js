f23769ce196ce30bf2e1bb80f66e922de2647a5b
W. James Dittmar <wjdittmar@gmail.com>
2024-03-25 16:12:33 -0700

    move tokenExtractor to middleware

--- part4/app.js
+++ part4/app.js
@@ -12,7 +12,7 @@ const middleware = require('./utils/middleware')
 
 app.use(cors())
 app.use(express.json())
-
+app.use(middleware.tokenExtractor)
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
