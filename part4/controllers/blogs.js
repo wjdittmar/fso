982b8b4597aedf20270eaa04ab716a5e9eb69edd
W. James Dittmar <wjdittmar@gmail.com>
2024-03-29 08:51:50 -0700

    make sure the put response populates the response with the corresponding user information

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -57,10 +57,9 @@ blogsRouter.put('/:id', async (request, response, next) => {
         author: body.author,
         url: body.url,
         likes: body.likes,
-        user: user.id
     }
 
-    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true }).populate('user', { username: 1, name: 1 })
     response.json(updatedBlog);
 });
 
