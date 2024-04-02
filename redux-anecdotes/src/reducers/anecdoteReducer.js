58efa73114e04e1111268e9c2ee458a6ab0d6d48
W. James Dittmar <wjdittmar@gmail.com>
2024-04-02 16:36:14 -0700

    fetch / store anecdotes on the json server

--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,31 +1,13 @@
 import { createSlice } from '@reduxjs/toolkit'
-const anecdotesAtStart = [
-	'If it hurts, do it more often',
-	'Adding manpower to a late software project makes it later!',
-	'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-	'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-	'Premature optimization is the root of all evil.',
-	'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
-const getId = () => (100000 * Math.random()).toFixed(0)
-
-const asObject = (anecdote) => {
-	return {
-		content: anecdote,
-		id: getId(),
-		votes: 0
-	}
-}
-
-const initialState = anecdotesAtStart.map(asObject)
 
 const anecdoteSlice = createSlice({
 	name: 'anecdote',
-	initialState,
+	initialState: [],
 	reducers: {
 		createAnecdote(state, action) {
-			state.push(asObject(action.payload));
+			// now called from the anecdote service which returns the anecdote object
+			// so no longer need to call asObject
+			state.push(action.payload);
 		},
 		voteAnecdote(state, action) {
 			const id = action.payload;
@@ -36,9 +18,12 @@ const anecdoteSlice = createSlice({
 			return state.map(anecdote =>
 				anecdote.id !== id ? anecdote : changedAnecdote
 			);
+		},
+		setAnecdotes(state, action) {
+			return action.payload;
 		}
 	},
 });
 
-export const { createAnecdote, voteAnecdote } = anecdoteSlice.actions
+export const { createAnecdote, voteAnecdote, setAnecdotes } = anecdoteSlice.actions
 export default anecdoteSlice.reducer
