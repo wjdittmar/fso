a764e762cbfa62b1499416b0f64fd433a08776d6
W. James Dittmar <wjdittmar@gmail.com>
2024-04-03 14:38:31 -0700

    7.4

--- part7_exercises/src/App.jsx
+++ part7_exercises/src/App.jsx
@@ -4,6 +4,8 @@ import {
 	Routes, Route, Link, useParams, useNavigate
 } from 'react-router-dom'
 
+import { useField } from './hooks'
+
 const Menu = () => {
 	const padding = {
 		paddingRight: 5
@@ -66,10 +68,9 @@ const Footer = () => (
 
 const CreateNew = (props) => {
 	const navigate = useNavigate()
-	const [content, setContent] = useState('')
-	const [author, setAuthor] = useState('')
-	const [info, setInfo] = useState('')
-
+	const content = useField('')
+	const author = useField('')
+	const info = useField('')
 
 	const handleSubmit = (e) => {
 		e.preventDefault()
@@ -88,15 +89,15 @@ const CreateNew = (props) => {
 			<form onSubmit={handleSubmit}>
 				<div>
 					content
-					<input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+					<input {...content} />
 				</div>
 				<div>
 					author
-					<input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+					<input {...author} />
 				</div>
 				<div>
 					url for more info
-					<input name='info' value={info} onChange={(e) => setInfo(e.target.value)} />
+					<input {...info} />
 				</div>
 				<button>create</button>
 			</form>
