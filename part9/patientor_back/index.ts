df6210da22bbb32b43c0da1be4cdbece4087108b
W. James Dittmar <wjdittmar@gmail.com>
2024-04-05 13:41:16 -0700

    allow cross origin resource sharing so the front end can make requests to the backend

--- part9/patientor_back/index.ts
+++ part9/patientor_back/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 const app = express();
 app.use(express.json());
+import cors from 'cors';
 
-const PORT = 3000;
+app.use(cors())
+const PORT = 3001;
 
-app.get('/ping', (_req, res) => {
+app.get('/api/ping', (_req, res) => {
 	console.log('someone pinged here');
 	res.send('pong');
 });
