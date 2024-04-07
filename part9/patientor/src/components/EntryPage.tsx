7327fec53b3c6b370b9495caea22728e1169ba9f
W. James Dittmar <wjdittmar@gmail.com>
2024-04-07 14:19:09 -0700

    9.24 fetch corresponding diagnosis codes for each patient

--- part9/patientor/src/components/EntryPage.tsx
+++ part9/patientor/src/components/EntryPage.tsx
@@ -1,10 +1,30 @@
-import { Entry } from "../types";
+import { useEffect, useState } from "react";
+import { Diagnosis, Entry } from "../types";
+import diagnosesService from '../services/diagnoses';
+
 const EntryPage = ({ entry }: { entry: Entry }) => {
+
+	const [diagnoses, setDiagnoses] = useState<Array<Diagnosis>>([]);
+
+	useEffect(() => {
+		void diagnosesService.getAllDiagnoses().then(data => {
+			setDiagnoses(data);
+		});
+	}, []);
+	console.log(diagnoses);
+	console.log(diagnoses.find((ele) => ele.code === 'M24.2'));
 	return (
 		<>
 			<p> {entry.date} <em> {entry.description}</em></p>
-			<ul>{entry.diagnosisCodes?.map((diagnosis) => <li> {diagnosis} </li>)}</ul>
+			<ul>
+				{entry.diagnosisCodes?.map((diagnosisCode) => (
+					<li key={diagnosisCode}>
+						{diagnosisCode} -{' '}
+						{diagnoses.find((diagnosis) => diagnosis.code === diagnosisCode)?.name}
+					</li>
+				))}
+			</ul>
 		</>);
-};
+}
 
 export default EntryPage;
---
+++ part9/patientor/src/services/diagnoses.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+import { Diagnosis } from "../types";
+import { apiBaseUrl } from "../constants";
+
+
+const getAllDiagnoses = async () => {
+	const { data } = await axios.get<Diagnosis>(
+		`${apiBaseUrl}/diagnoses/`
+	);
+
+	return data;
+};
+
+
+export default {
+	getAllDiagnoses
+};
+
